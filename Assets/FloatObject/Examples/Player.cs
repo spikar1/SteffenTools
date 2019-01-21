52f312ab51d1604c19dd0e59117a00f74dd3043a
spikar1 <Steffen.fureli@gmail.com>
2019-01-21 23:03:24 +0100

    Minor changes

--- Assets/FloatObject/Examples/Player.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using SteffenTools.FloatObject;
-
-public class Player : MonoBehaviour
-{
-    public FloatObject playerSpeed;
-}
