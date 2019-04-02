31e51c96d79a799a605810cda2955ebbe6672e16
spikar1 <Steffen.fureli@gmail.com>
2019-04-02 13:49:23 +0200

    Added a node network. Not made much

---
+++ Assets/NodeNetwork/Network.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NodeNetwork
+{
+    public class Network : MonoBehaviour
+    {
+        public List<Node> nodes = new List<Node>();
+
+        private void Start()
+        {
+            var mainNode = new Node(null, Vector3.zero, 1);
+            nodes.Add(mainNode);
+
+            var lastNode = mainNode;
+            for (int i = 0; i < 10; i++)
+            {
+                var newNode = new Node(lastNode, lastNode.pos + Vector3.forward * lastNode.size * 2, lastNode.size * .9f);
+                nodes.Add(newNode);
+                lastNode = newNode;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            foreach (var node in nodes)
+            {
+                Gizmos.DrawSphere(node.pos, node.size);
+                foreach (var neighbour in node.neighbours)
+                {
+                    Gizmos.DrawLine(node.pos, neighbour.pos);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/NodeNetwork/Node.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NodeNetwork
+{
+    [System.Serializable]
+    public class Node 
+    {
+        public List<Node> neighbours = new List<Node>();
+        public int distance;
+        public Vector3 pos;
+        public float size;
+
+        public Node(Node parent, Vector3 pos, float size = 1)
+        {
+            if(parent != null)
+                neighbours.Add(parent);
+            this.pos = pos;
+            this.size = size;
+        }
+    }
+}
