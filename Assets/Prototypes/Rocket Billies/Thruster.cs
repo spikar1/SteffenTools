ec846ce74683aecddb80d149bb57294fb5e4d019
spikar1 <Steffen.fureli@gmail.com>
2019-09-18 08:56:30 +0200

    Added prototype of Rocket Billies

---
+++ Assets/Prototypes/Rocket Billies/Thruster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thruster : MonoBehaviour
+{
+    public KeyCode thrustKey = KeyCode.Space;
+    public Rigidbody2D shipRb;
+    public float thrusterPower = 2;
+
+    private void Start()
+    {
+        if (!shipRb)
+            shipRb = transform.GetComponentInParent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(thrustKey))
+        {
+            shipRb.AddForceAtPosition(transform.up * thrusterPower, transform.position);
+        }
+    }
+}
