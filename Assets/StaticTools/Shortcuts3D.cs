b864baf723d1eef5095a0051d3e079037f97c536
Steffen <steffen.fureli@gmail.com>
2020-03-13 15:09:37 +0100

    Adds a PlaceOnGround Shortcut

---
+++ Assets/StaticTools/Shortcuts3D.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace SteffenTools.Shortcuts.For3D {
+    public class Shortcuts {
+
+        [MenuItem("SteffenTools/Shortcuts/3D/PlaceOnGround #S")]
+        public static void PlaceOnGround() {
+            foreach (var trans in Selection.transforms) {
+                DoPlaceOnGround(trans);
+            }
+        }
+
+        static void DoPlaceOnGround(Transform trans) {
+            Undo.RecordObject(trans, "Placed " + trans.name + " on the ground");
+            var renderer = trans.GetComponent<Renderer>();
+
+            var offset = 0f;
+            if (renderer.GetComponent<SpriteRenderer>())
+                //TODO: Find a way to get the bottom pixel colour 
+                {
+                offset = 0f;
+            }
+            else if (renderer)
+                offset = renderer.bounds.extents.y;
+
+            //TODO: Should have solution not revolving around colliders, but alas
+            RaycastHit hit;
+            RaycastHit2D hit2D;
+
+            if (Physics.Raycast(trans.position, Vector3.down, out hit, 100)) {
+                trans.position = hit.point + Vector3.up * offset;
+            }
+            else if (hit2D = Physics2D.Raycast(trans.position, Vector2.down, 100)) {
+                trans.position = hit2D.point + Vector2.up * offset;
+            }
+            EditorUtility.SetDirty(trans);
+        }
+
+        //TODO: Make PlaceOnGroundWithAlign. This needs a seperate function.
+    }
+}
