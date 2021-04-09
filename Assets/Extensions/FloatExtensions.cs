910d5ee33ceb8911e856b16c4f7fe53007cfcb69
Steffen M. Fureli <Steffen.fureli@gmail.com>
2021-04-09 09:35:57 +0200

    Add namespace

--- Assets/Extensions/FloatExtensions.cs
+++ Assets/Extensions/FloatExtensions.cs
@@ -1,31 +1,33 @@
 using UnityEngine;
 using System.Collections;
 
-public static class FloatExtensions {
+namespace SteffenTools{
+    public static class FloatExtensions {
 
-    public static bool IsOdd(this float myfloat) {
-        if (Mathf.Abs(myfloat) % 2 == 1) {
-            return true;
+        public static bool IsOdd(this float myfloat) {
+            if (Mathf.Abs(myfloat) % 2 == 1) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
-        else {
-            return false;
-        }
-    }
 
-    public static bool IsEven(this float myfloat) {
-        if (Mathf.Abs(myfloat) % 2 == 0) {
-            return true;
+        public static bool IsEven(this float myfloat) {
+            if (Mathf.Abs(myfloat) % 2 == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
-        else {
-            return false;
+        public static int Sign(this float myfloat) {
+            if (myfloat < 0)
+                return -1;
+            else if (myfloat > 0)
+                return 1;
+            else
+                return 0;
         }
-    }
-    public static int Sign(this float myfloat) {
-        if (myfloat < 0)
-            return -1;
-        else if (myfloat > 0)
-            return 1;
-        else
-            return 0;
     }
 }
